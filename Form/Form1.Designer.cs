507d5a7d8e68d02fd48298376fd261d1163e1d87
EsraKIZILELMA <kizilelmaesra@gmail.com>
2021-12-20 14:25:29 +0300

    form

---
+++ Form/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace veritabanideneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSil = new System.Windows.Forms.Button();
+            this.buttonGuncelle = new System.Windows.Forms.Button();
+            this.buttonAra = new System.Windows.Forms.Button();
+            this.buttonListele = new System.Windows.Forms.Button();
+            this.textBoxStokAdet = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIlacAdi = new System.Windows.Forms.TextBox();
+            this.textBoxTETT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFirmaAdi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAlisFiyati = new System.Windows.Forms.TextBox();
+            this.buttonEkle = new System.Windows.Forms.Button();
+            this.StokAdet = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxilacid = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSatisfiyati = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSil
+            // 
+            this.buttonSil.BackColor = System.Drawing.Color.PapayaWhip;
+            this.buttonSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSil.ForeColor = System.Drawing.Color.Navy;
+            this.buttonSil.Location = new System.Drawing.Point(548, 100);
+            this.buttonSil.Name = "buttonSil";
+            this.buttonSil.Size = new System.Drawing.Size(266, 45);
+            this.buttonSil.TabIndex = 1;
+            this.buttonSil.Text = "Sil";
+            this.buttonSil.UseVisualStyleBackColor = false;
+            this.buttonSil.Click += new System.EventHandler(this.buttonSil_Click);
+            // 
+            // buttonGuncelle
+            // 
+            this.buttonGuncelle.BackColor = System.Drawing.Color.PapayaWhip;
+            this.buttonGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGuncelle.ForeColor = System.Drawing.Color.Navy;
+            this.buttonGuncelle.Location = new System.Drawing.Point(548, 173);
+            this.buttonGuncelle.Name = "buttonGuncelle";
+            this.buttonGuncelle.Size = new System.Drawing.Size(266, 45);
+            this.buttonGuncelle.TabIndex = 2;
+            this.buttonGuncelle.Text = "Güncelle";
+            this.buttonGuncelle.UseVisualStyleBackColor = false;
+            this.buttonGuncelle.Click += new System.EventHandler(this.buttonGuncelle_Click);
+            // 
+            // buttonAra
+            // 
+            this.buttonAra.BackColor = System.Drawing.Color.PapayaWhip;
+            this.buttonAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonAra.ForeColor = System.Drawing.Color.Navy;
+            this.buttonAra.Location = new System.Drawing.Point(548, 250);
+            this.buttonAra.Name = "buttonAra";
+            this.buttonAra.Size = new System.Drawing.Size(266, 45);
+            this.buttonAra.TabIndex = 3;
+            this.buttonAra.Text = "Ara";
+            this.buttonAra.UseVisualStyleBackColor = false;
+            this.buttonAra.Click += new System.EventHandler(this.buttonAra_Click);
+            // 
+            // buttonListele
+            // 
+            this.buttonListele.BackColor = System.Drawing.Color.PapayaWhip;
+            this.buttonListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonListele.ForeColor = System.Drawing.Color.Navy;
+            this.buttonListele.Location = new System.Drawing.Point(548, 320);
+            this.buttonListele.Name = "buttonListele";
+            this.buttonListele.Size = new System.Drawing.Size(266, 45);
+            this.buttonListele.TabIndex = 4;
+            this.buttonListele.Text = "Listele";
+            this.buttonListele.UseVisualStyleBackColor = false;
+            this.buttonListele.Click += new System.EventHandler(this.buttonListele_Click);
+            // 
+            // textBoxStokAdet
+            // 
+            this.textBoxStokAdet.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxStokAdet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxStokAdet.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxStokAdet.Location = new System.Drawing.Point(192, 180);
+            this.textBoxStokAdet.Multiline = true;
+            this.textBoxStokAdet.Name = "textBoxStokAdet";
+            this.textBoxStokAdet.Size = new System.Drawing.Size(210, 36);
+            this.textBoxStokAdet.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.PapayaWhip;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 379);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(998, 369);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Location = new System.Drawing.Point(50, 79);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(6);
+            this.label2.Size = new System.Drawing.Size(94, 37);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "İlaç Adı:";
+            // 
+            // textBoxIlacAdi
+            // 
+            this.textBoxIlacAdi.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxIlacAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxIlacAdi.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxIlacAdi.Location = new System.Drawing.Point(191, 80);
+            this.textBoxIlacAdi.Multiline = true;
+            this.textBoxIlacAdi.Name = "textBoxIlacAdi";
+            this.textBoxIlacAdi.Size = new System.Drawing.Size(209, 36);
+            this.textBoxIlacAdi.TabIndex = 8;
+            // 
+            // textBoxTETT
+            // 
+            this.textBoxTETT.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxTETT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxTETT.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxTETT.Location = new System.Drawing.Point(192, 130);
+            this.textBoxTETT.Multiline = true;
+            this.textBoxTETT.Name = "textBoxTETT";
+            this.textBoxTETT.Size = new System.Drawing.Size(209, 36);
+            this.textBoxTETT.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label4.Location = new System.Drawing.Point(51, 230);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(6);
+            this.label4.Size = new System.Drawing.Size(118, 37);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Firma Adı :";
+            // 
+            // textBoxFirmaAdi
+            // 
+            this.textBoxFirmaAdi.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxFirmaAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxFirmaAdi.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxFirmaAdi.Location = new System.Drawing.Point(191, 230);
+            this.textBoxFirmaAdi.Multiline = true;
+            this.textBoxFirmaAdi.Name = "textBoxFirmaAdi";
+            this.textBoxFirmaAdi.Size = new System.Drawing.Size(209, 36);
+            this.textBoxFirmaAdi.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label5.Location = new System.Drawing.Point(50, 280);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(6);
+            this.label5.Size = new System.Drawing.Size(118, 37);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Alış Fiyatı :";
+            // 
+            // textBoxAlisFiyati
+            // 
+            this.textBoxAlisFiyati.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxAlisFiyati.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxAlisFiyati.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxAlisFiyati.Location = new System.Drawing.Point(190, 280);
+            this.textBoxAlisFiyati.Multiline = true;
+            this.textBoxAlisFiyati.Name = "textBoxAlisFiyati";
+            this.textBoxAlisFiyati.Size = new System.Drawing.Size(209, 36);
+            this.textBoxAlisFiyati.TabIndex = 14;
+            // 
+            // buttonEkle
+            // 
+            this.buttonEkle.BackColor = System.Drawing.Color.PapayaWhip;
+            this.buttonEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkle.ForeColor = System.Drawing.Color.Navy;
+            this.buttonEkle.Location = new System.Drawing.Point(548, 30);
+            this.buttonEkle.Name = "buttonEkle";
+            this.buttonEkle.Size = new System.Drawing.Size(266, 45);
+            this.buttonEkle.TabIndex = 16;
+            this.buttonEkle.Text = "Ekle";
+            this.buttonEkle.UseVisualStyleBackColor = false;
+            this.buttonEkle.Click += new System.EventHandler(this.buttonEkle_Click);
+            // 
+            // StokAdet
+            // 
+            this.StokAdet.AutoSize = true;
+            this.StokAdet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.StokAdet.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.StokAdet.Location = new System.Drawing.Point(51, 180);
+            this.StokAdet.Name = "StokAdet";
+            this.StokAdet.Padding = new System.Windows.Forms.Padding(6);
+            this.StokAdet.Size = new System.Drawing.Size(116, 37);
+            this.StokAdet.TabIndex = 17;
+            this.StokAdet.Text = "Stok Adet:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Location = new System.Drawing.Point(50, 29);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(6);
+            this.label1.Size = new System.Drawing.Size(84, 37);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "İlaç ID:";
+            // 
+            // textBoxilacid
+            // 
+            this.textBoxilacid.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxilacid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxilacid.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxilacid.Location = new System.Drawing.Point(191, 30);
+            this.textBoxilacid.Multiline = true;
+            this.textBoxilacid.Name = "textBoxilacid";
+            this.textBoxilacid.Size = new System.Drawing.Size(209, 36);
+            this.textBoxilacid.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label6.Location = new System.Drawing.Point(50, 329);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(6);
+            this.label6.Size = new System.Drawing.Size(125, 37);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Satış Fiyatı:";
+            // 
+            // textBoxSatisfiyati
+            // 
+            this.textBoxSatisfiyati.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBoxSatisfiyati.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSatisfiyati.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxSatisfiyati.Location = new System.Drawing.Point(191, 330);
+            this.textBoxSatisfiyati.Multiline = true;
+            this.textBoxSatisfiyati.Name = "textBoxSatisfiyati";
+            this.textBoxSatisfiyati.Size = new System.Drawing.Size(209, 36);
+            this.textBoxSatisfiyati.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label3.Location = new System.Drawing.Point(50, 130);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(6);
+            this.label3.Size = new System.Drawing.Size(82, 37);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "TETT:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(103)))), ((int)(((byte)(99)))));
+            this.ClientSize = new System.Drawing.Size(1073, 801);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxSatisfiyati);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxilacid);
+            this.Controls.Add(this.StokAdet);
+            this.Controls.Add(this.buttonEkle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxAlisFiyati);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxFirmaAdi);
+            this.Controls.Add(this.textBoxTETT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxIlacAdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxStokAdet);
+            this.Controls.Add(this.buttonListele);
+            this.Controls.Add(this.buttonAra);
+            this.Controls.Add(this.buttonGuncelle);
+            this.Controls.Add(this.buttonSil);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSil;
+        private System.Windows.Forms.Button buttonGuncelle;
+        private System.Windows.Forms.Button buttonAra;
+        private System.Windows.Forms.Button buttonListele;
+        private System.Windows.Forms.TextBox textBoxStokAdet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIlacAdi;
+        private System.Windows.Forms.TextBox textBoxTETT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFirmaAdi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAlisFiyati;
+        private System.Windows.Forms.Button buttonEkle;
+        private System.Windows.Forms.Label StokAdet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxilacid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSatisfiyati;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form/Form1.cs
@@ -0,0 +1,109 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veritabanideneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; " +
+            "port=5432; Database=eczane; user ID=postgres; password=b191210040");
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEkle_Click(object sender, EventArgs e)
+        {
+            DateTime time;
+            baglanti.Open();
+            NpgsqlCommand ekle = new NpgsqlCommand("insert into ilac(ilacid,ilacadi,skt,stokadet,tedarikci,alisfiyati,satisfiyati)" +
+                " values (@ilacid,@ilacadi,@skt,@stokadet,@tedarikci,@alisfiyati,@satisfiyati)", baglanti);
+            ekle.Parameters.AddWithValue("@ilacid", int.Parse(textBoxilacid.Text));
+            ekle.Parameters.AddWithValue("@ilacadi", textBoxIlacAdi.Text);
+            ekle.Parameters.AddWithValue("@skt", textBoxTETT.Text);
+            ekle.Parameters.AddWithValue("@stokadet", int.Parse(textBoxStokAdet.Text));
+            ekle.Parameters.AddWithValue("@tedarikci", textBoxFirmaAdi.Text);
+            ekle.Parameters.AddWithValue("@alisfiyati", int.Parse(textBoxAlisFiyati.Text));
+            ekle.Parameters.AddWithValue("@satisfiyati", int.Parse(textBoxSatisfiyati.Text));
+            ekle.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Ilac kaydı başarılı bir sekilde gerçekleşti", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+        }
+
+        private void buttonListele_Click(object sender, EventArgs e)
+        {
+            string sorgu_ilac = "select * from ilac";
+            NpgsqlDataAdapter datadapter = new NpgsqlDataAdapter(sorgu_ilac, baglanti);
+            DataSet dataset = new DataSet();
+            datadapter.Fill(dataset);
+            dataGridView1.DataSource = dataset.Tables[0];
+        }
+
+        private void buttonSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand sil = new NpgsqlCommand("delete from ilac where ilacid=@ilacid", baglanti);
+            sil.Parameters.AddWithValue("@ilacid", int.Parse(textBoxilacid.Text));
+            sil.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Silme işlemi başarılı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void buttonGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand guncelle = new NpgsqlCommand("update ilac set ilacadi = @ilacadi, skt = @skt, stokadet = @stokadet," +
+                " tedarikci = @tedarikci, alisfiyati = @alisfiyati, satisfiyati = @satisfiyati where ilacid = @ilacid", baglanti);
+            guncelle.Parameters.AddWithValue("@ilacid", int.Parse(textBoxilacid.Text));
+            guncelle.Parameters.AddWithValue("@ilacadi", textBoxIlacAdi.Text);
+            guncelle.Parameters.AddWithValue("@skt", textBoxTETT.Text);
+            guncelle.Parameters.AddWithValue("@stokadet", int.Parse(textBoxStokAdet.Text));
+            guncelle.Parameters.AddWithValue("@tedarikci", textBoxFirmaAdi.Text);
+            guncelle.Parameters.AddWithValue("@alisfiyati", int.Parse(textBoxAlisFiyati.Text));
+            guncelle.Parameters.AddWithValue("@satisfiyati", int.Parse(textBoxSatisfiyati.Text));
+            guncelle.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Güncelleme işlemi başarılı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void buttonAra_Click(object sender, EventArgs e)
+        {
+            if(textBoxilacid.Text != string.Empty)
+            {
+                baglanti.Open();
+                string sorgu = "select*from ilac where ilacid =" + textBoxilacid.Text;
+                NpgsqlDataAdapter araID = new NpgsqlDataAdapter(sorgu, baglanti);
+                DataSet dataset = new DataSet();
+                araID.Fill(dataset);
+                dataGridView1.DataSource = dataset.Tables[0];
+                baglanti.Close();
+            }
+            if (textBoxIlacAdi.Text != string.Empty)
+            {
+                baglanti.Open();
+                string sorgu = "select*from ilac where ilacid =" + textBoxIlacAdi.Text;
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                baglanti.Close();
+            }
+        }
+    }
+    
+}
